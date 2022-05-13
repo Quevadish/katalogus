7a946c19efe1db04ce81804c62461a8ee6d3dcfc
Porubsky Mario <majoka1234561@gmail.com>
2022-05-13 13:48:19 +0200

    Initial commit

---
+++ src/main/java/epam/progkor/katalogus/katalogus/KatalogusApplication.java
@@ -0,0 +1,13 @@
+package epam.progkor.katalogus.katalogus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatalogusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatalogusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epam/progkor/katalogus/katalogus/KatalogusApplicationTests.java
@@ -0,0 +1,13 @@
+package epam.progkor.katalogus.katalogus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatalogusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
